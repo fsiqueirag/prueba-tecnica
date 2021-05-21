0de2a13e26dae3a288d9304decc593d6f5b9b00e
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2021-05-21 16:40:44 -0300

    corregir y retocar estilos y mejorar legibilidad del código

--- Code/r3/R3.js
+++ Code/r3/R3.js
@@ -1,18 +1,26 @@
-let image = document.getElementById("image-container");
-let modal = document.getElementById("modal-container");
-let mainContent = document.getElementById("main-content");
-let closeModal = document.getElementById("close-modal");
-let iframe = document.querySelector( 'iframe');
+// Obtener elementos
 
-let stopVideo = () => {
+const image = document.getElementById("image-container");
+const modal = document.getElementById("modal-container");
+const mainContent = document.getElementById("main-content");
+const closeModal = document.getElementById("close-modal");
+const iframe = document.querySelector( 'iframe');
+
+// Declarar función que detiene el video
+
+const stopVideo = () => {
 	let iframeSrc = iframe.src;
 	iframe.src = iframeSrc;
 };
+
+// Mostrar modal al clickear imagen
 
 image.addEventListener('click', () => {
     modal.classList.add('shown');
     mainContent.classList.add('blur');
 });
+
+// Cerrar modal
 
 closeModal.addEventListener('click', () => {
     stopVideo();
